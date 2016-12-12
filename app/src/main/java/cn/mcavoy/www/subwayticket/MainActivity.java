2bf8c5c01352f9c85b65c5626669a9db833d8285
yyijian <yyijian601@gmail.com>
2016-12-12 22:23:06 +0800

    add Index Ui

--- app/src/main/java/cn/mcavoy/www/subwayticket/MainActivity.java
+++ app/src/main/java/cn/mcavoy/www/subwayticket/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
