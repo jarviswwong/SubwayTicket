b48afc1e89edebe5a37b02fc720f34989b436d33
yyijian <yyijian601@gmail.com>
2016-12-14 19:23:15 +0800

    use repositories 'IndexRecyclerView'

--- app/src/main/java/cn/mcavoy/www/subwayticket/OriginStationListActivity.java
+++ app/src/main/java/cn/mcavoy/www/subwayticket/OriginStationListActivity.java
@@ -4,17 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OriginStationListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter MyAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.origin_station_list_main);
         initToolbar();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        MyAdapter = new RecyclerViewAdapter(OriginStationListActivity.this, initData());
+        recyclerView.setAdapter(MyAdapter);
+    }
+
+    private List<String> initData() {
+        List<String> datas = new ArrayList<String>();
+        for (int i = 0; i <= 20; i++) {
+            datas.add("item:" + i);
+        }
+        return datas;
     }
 
     private void initToolbar() {
